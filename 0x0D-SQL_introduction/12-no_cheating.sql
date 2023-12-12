8bfa2f346a22eebe6cc1bbb5fb19acd9540b5e5d
tabi5 <tinbitdemissie@gmail.com>
2023-12-12 09:02:14 -0800

    task12

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,5 +1,2 @@
--- update score of Bob to 10
-UPDATE `second_table`
-SET
-`score` = 10
-WHERE `second_table` . `name` = 'Bob';
+-- update score of Bob to 10 in 'second_table'
+UPDATE `second_table` SET `score` = 10 WHERE `name` = "Bob";
